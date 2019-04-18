ec7424b8cabc702b5d3323e0d3135dcf5cdb7907
Artur Kondas <devilyoushy@gmail.com>
2019-04-18 15:34:06 +0100

    Update main.js
    New date!

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const count = {}
-    count.toDate = new Date("Oct 13, 2018, 20:00:00").getTime()
+    count.toDate = new Date("Apr 29, 2019, 12:00:00").getTime()
     count.setDate = document.getElementById("setdate").innerHTML = "Waiting for the trip:"
 
     let interval = setInterval(() => {
